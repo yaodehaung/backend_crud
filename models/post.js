6401c27ea90b6eaf072d7c18f3c3161067a3f089
yaodehuang <sef1548@gmail.com>
2016-04-25 21:58:47 +0800

    firt commit this project

---
+++ models/post.js
@@ -0,0 +1,220 @@
+var mongodb = require('./db'),
+  markdown = require('markdown').markdown,
+  ObjectID = require('mongodb').ObjectID;
+
+function Post(name, head, title, tags, post) {
+  this.name = name;
+  this.head = head;
+  this.title = title;
+  this.tags = tags;
+  this.post = post;
+}
+
+module.exports = Post;
+
+//存储一篇文章及其相关信息
+Post.prototype.save = function(callback) {
+  var date = new Date();
+  //存储各种时间格式，方便以后扩展
+  var time = {
+      date: date,
+      year: date.getFullYear(),
+      month: date.getFullYear() + "-" + (date.getMonth() + 1),
+      day: date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate(),
+      minute: date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +
+        date.getHours() + ":" + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes())
+    }
+    //要存入数据库的文档
+  var post = {
+    name: this.name,
+    head: this.head,
+    time: time,
+    title: this.title,
+    tags: this.tags,
+    post: this.post,
+    comments: [],
+    reprint_info: {},
+    pv: 0
+  };
+  //打开数据库
+  mongodb.open(function(err, db) {
+    if (err) {
+      return callback(err);
+    }
+    //读取 posts 集合
+    db.collection('posts', function(err, collection) {
+      if (err) {
+        mongodb.close();
+        return callback(err);
+      }
+      //将文档插入 posts 集合
+      collection.insert(post, {
+        safe: true
+      }, function(err) {
+        mongodb.close();
+        if (err) {
+          return callback(err); //失败！返回 err
+        }
+        callback(null); //返回 err 为 null
+      });
+    });
+  });
+};
+
+
+Post.edit = function(name, callback) {
+  //打开数据库
+  mongodb.open(function(err, db) {
+    if (err) {
+      return callback(err);
+    }
+    //读取 posts 集合
+    db.collection('posts', function(err, collection) {
+      if (err) {
+        mongodb.close();
+        return callback(err);
+      }
+      //根据用户名、发表日期及文章名进行查询
+      collection.findOne({
+        "name": name,
+      }, function(err, doc) {
+        mongodb.close();
+        if (err) {
+          return callback(err);
+        }
+        callback(null, doc); //返回查询的一篇文章（markdown 格式）
+      });
+    });
+  });
+};
+
+
+//更新一篇文章及其相关信息
+Post.update = function(name, post, callback) {
+  //打开数据库
+  mongodb.open(function(err, db) {
+    if (err) {
+      return callback(err);
+    }
+    //读取 posts 集合
+    db.collection('posts', function(err, collection) {
+      var o_id = new ObjectID(name);
+      if (err) {
+        mongodb.close();
+        return callback(err);
+      }
+      //更新文章内容
+      collection.update({
+        "_id": o_id
+      }, {
+        $set: {
+          title: post
+        }
+      }, function(err) {
+        mongodb.close();
+        if (err) {
+          return callback(err);
+        }
+        callback(null);
+      });
+    });
+  });
+};
+
+//删除一篇文章
+Post.remove = function(name, callback) {
+  //打开数据库
+  mongodb.open(function(err, db) {
+    if (err) {
+      return callback(err);
+    }
+    //读取 posts 集合
+    db.collection('posts', function(err, collection) {
+      var o_id = new ObjectID(name);
+      if (err) {
+        mongodb.close();
+        return callback(err);
+      }
+      //查询要删除的文档
+      collection.findOne({
+        "_id" : o_id
+      }, function(err, doc) {
+        if (err) {
+          mongodb.close();
+          return callback(err);
+        }
+        //删除转载来的文章所在的文档
+        collection.remove({
+          "_id" : o_id
+        }, {
+          w: 1
+        }, function(err) {
+          mongodb.close();
+          if (err) {
+            return callback(err);
+          }
+          callback(null);
+        });
+      });
+    });
+  });
+};
+
+//返回所有文章存档信息
+Post.getArchive = function(callback) {
+  //打开数据库
+  mongodb.open(function(err, db) {
+    if (err) {
+      return callback(err);
+    }
+    //读取 posts 集合
+    db.collection('posts', function(err, collection) {
+      if (err) {
+        mongodb.close();
+        return callback(err);
+      }
+      //返回只包含 name、time、title 属性的文档组成的存档数组
+      collection.find({}, {
+        "name": 1,
+        "time": 1,
+        "title": 1
+      }).sort({
+        time: -1
+      }).toArray(function(err, docs) {
+        mongodb.close();
+        if (err) {
+          return callback(err);
+        }
+        callback(null, docs);
+      });
+    });
+  });
+};
+
+Post.getOne = function(name, callback) {
+  //打开数据库
+
+  mongodb.open(function(err, db) {
+    if (err) {
+      return callback(err);
+    }
+    //读取 posts 集合
+    db.collection('posts', function(err, collection) {
+      var o_id = new ObjectID(name);
+      if (err) {
+        mongodb.close();
+        return callback(err);
+      }
+      //返回只包含 name、time、title 属性的文档组成的存档数组
+      collection.find({
+        "_id": o_id
+      }).toArray(function(err, docs) {
+        mongodb.close();
+        if (err) {
+          return callback(err);
+        }
+        callback(null, docs);
+      });
+    });
+  });
+};
